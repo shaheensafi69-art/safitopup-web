3ff385bae960b8b1a2193c79b800aff9d10264c3
Shaheen Safi <ssafi9241@gmail.com>
2026-03-26 23:26:04 +0430

    feat: auto-redirect to EN and core layout setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en', // یا هر صفحه‌ای که صفحه اصلی شماست
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
